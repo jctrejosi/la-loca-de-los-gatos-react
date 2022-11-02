708d6dbbebd136526234774f8d6899e44892c1ec
Juan Trejos <juan.trejos@roiback.com>
2022-11-02 17:55:50 -0500

    Use contents directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import Header from "#/Header/Header";
 import Footer from "#/Footer/Footer";
-import logoSvg from "./assets/images/logo.svg";
-
-const web = import.meta.env.VITE_SERVER;
-
-const logo = logoSvg;
-
-const slogan = "la loca de los gatos";
+import { navbarlist, web, logo, slogan } from "contents/header";
 
 function App() {
   return (
     <div className="App">
-      <Header url={web} image={logo} slogan={slogan} />
+      <Header url={web} image={logo} slogan={slogan} items={navbarlist} />
       <Footer />
     </div>
   );
