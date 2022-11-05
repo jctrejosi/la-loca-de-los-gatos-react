8652e07189839090810bdfe0176da79423b8ecd8
Juan Trejos <juan.trejos@roiback.com>
2022-11-05 18:06:02 -0500

    fix z-index in header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,7 +12,7 @@ interface props {
 
 function Header({ url, image, slogan, items }: props): ReactElement {
   return (
-    <header className="bg-circle bg-center bg-fixed bg-cover flex justify-between lg:justify-around px-8 items-center">
+    <header className="bg-circle bg-center bg-fixed bg-cover flex justify-between lg:justify-around px-8 items-center z-50 relative">
       <Icon url={url} image={image} slogan={slogan} />
       <Navbar list={items} />
     </header>
